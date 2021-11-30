c910f922e4023c642352c0d350efafbcaa61007f
stvalik <st.valentine71@gmail.com>
2021-11-30 15:58:19 +0200

    sdlgf

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const names= ['Sara', 'John','Fred']
+
+
+
+
+ 
+
+
+
+var arr = new Array(10).fill().map((u,index) => ({
+author:{
+	avatar:'img/avatars/user01.png',
+},
+offer:{
+	title:'title',
+  address: 'address',
+},
+location:{
+x: 10.01,
+y: 20.02,
+}
+}
+));
+console.log(arr)
